8456848bfeb4b7ed58a0da77dbd7e4a659d45904
pc <pmnarimani@gmail.com>
2026-02-21 14:29:48 +0100

    Tooltip

---
+++ Assets/Scripts/UI/Widgets/TooltipMaker.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace SiegeSurvival
+{
+    public class TooltipMaker : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+    {
+        [SerializeField, TextArea] private string _tooltip;
+
+
+        public void OnPointerEnter(PointerEventData eventData)
+        {
+            TooltipWidget.Instance.Show(_tooltip);
+        }
+
+        public void OnPointerExit(PointerEventData eventData)
+        {
+            TooltipWidget.Instance.Hide();
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Widgets/TooltipWidget.cs
@@ -0,0 +1,35 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+namespace SiegeSurvival
+{
+    public class TooltipWidget : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _text;
+        
+        public static TooltipWidget Instance { get; private set; }
+
+        void Awake()
+        {
+            Instance = this;
+            gameObject.SetActive(false);
+        }
+
+        void Update()
+        {
+            transform.position = Input.mousePosition;
+        }
+
+        public void Show(string tooltip)
+        {
+            _text.text = tooltip;
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+    }
+}
